f6929fab9285a51f2caa870600a21a553c5827b6
clayrock78 <clayrock78@gmail.com>
2022-11-16 19:32:46 +0000

    added room codes and join_room functionality

--- chessschedule/models/room.py
+++ chessschedule/models/room.py
@@ -1,3 +1,9 @@
+from uuid import uuid1
+from random import randint
 class Room:
-    def __init__(self, uuid:str) -> None:
-        self.uuid = uuid
+    def __init__(self, admin_uuid:str) -> None:
+        self.uuid = uuid1()
+        self.admin_uuid = admin_uuid
+
+        # TODO this is a temporary solution - the odds of a collision are low but possible
+        self.room_code = str(randint(1_000_000, 9_999_999)) 
--- chessschedule/routes/socket_routes.py
+++ chessschedule/routes/socket_routes.py
@@ -2,15 +2,32 @@ from ..app import skt
 from flask_socketio import emit, send
 from flask_socketio import join_room, leave_room
 from typing import List
+from ..models.room import Room
 
 """
 emit() has a kwarg called broadcast, which dictates sending to all users connected to the socket vs sending to just the
 requester. the default for emit is broadcast = True
 """
 
+def get_room_code(code:str):
+    for room in rooms:
+        if room.code == code:
+            return room
+    return None
+
+def get_room_uuid(uuid:str):
+    for room in rooms:
+        if room.uuid == uuid:
+            return room
+    return None
+
 @skt.on('connect')
 def connect(data):
-    emit('my response', {'dawdoiwahdiaow': 'Connected', "akey" : 12}, broadcast=False)
+    selected_room = get_room_code(data["code"]) or None
+    if not selected_room: return # TODO some error to user
+    join_room(selected_room.uuid)
+    # TODO emit needed information
+    emit('connect_res', {'Status': 'Connected', "akey" : 12}, broadcast=False)
 
 
 @skt.on('my event')
@@ -22,19 +39,18 @@ rooms: List[Room] = list()
 
 @skt.on("create_room")
 def create(data):
-    emit("response", {"user":"jsodaijfsdoifj", "room":"SOMEROOM"}, broadcast=False)
+    room = Room(data[user_uuid])
+    rooms.append(room)
+    # TODO emit needed information
+    emit("response", {"user":"TODO", "room":room.uuid}, broadcast=False)
 
 @skt.on("delete_room")
 def delete_room(data):
-    selected_room = None
-    for room in rooms:
-        if room.uuid == data["room_uuid"]:
-            selected_room = room
+    selected_room = get_room_uuid(data["room_uuid"]) or None   
 
     if not selected_room:
-        return # some kind of error handling
+        return # TODO some error to user
     
     if data["user_uuid"] == room.admin_uuid:
         # tell all clients to leave the room
-        emit("delete_room_res", 'Room closed by admin', broadcast=True)
-    
+        emit("delete_room_res", 'Room closed by admin', to=room.uuid)
