d96a9e23f254929bb67553f72268fa9eaa9aeece
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-11-17 01:08:56 +0000

    front end now can get room code

--- chessschedule/static/index.js
+++ chessschedule/static/index.js
@@ -1,9 +1,36 @@
-var socket = io();
-socket.on('connect', () => {
-    console.log("Connected!");
-    socket.emit('my event', {msg : "even cooler msg"});
-});
 
-socket.on('wow', (data) => {
-    console.log(data.nicemsg);
-});
+const socketBuilder = () => {
+    var socket = io();
+    socket.on('connect', () => {
+        console.log("Connected");
+    });
+    socket.on('create_room_res', (data) => {
+        localStorage.setItem('roomUUID', data.room_uuid);
+        localStorage.setItem('userUUID', data.user_uuid);
+        localStorage.setItem('roomCode', data.room_code);
+    });
+
+    return socket;
+}
+
+function socketManager () {
+    return {
+        socket: null,
+        roomUUID: null, // either will be undefined or the actaul uuid's
+        userUUID: null,
+        roomCode: null,
+        init() {
+            this.socket = socketBuilder();
+            this.roomUUID = localStorage.getItem('roomUUID');
+            this.userUUID= localStorage.getItem('userUUID');
+            this.roomCode = localStorage.getItem('roomCode');
+
+        },
+        createRoom() {
+            this.socket.emit('create_room', {});
+        },
+        reconnect() {
+
+        }
+    }
+}
