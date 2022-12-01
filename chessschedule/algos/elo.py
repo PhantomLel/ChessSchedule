fa4c34a44c9f7bd8b0584fcd80a8d15d03cc1f27
clayrock78 <clayrock78@gmail.com>
2022-12-01 18:24:16 +0000

    another dumb bug

--- chessschedule/algos/elo.py
+++ chessschedule/algos/elo.py
@@ -20,7 +20,7 @@ def change_rating(player1_rating, player2_rating, game_result) -> Tuple[int, int
     expected_score = 1 / (
         1 + 10 ** ((player2_rating - player1_rating) / ELO_COEFFICIENT)
     )
-    new_rating1 = round(player1_rating + K_FACTOR * (game_result - expected_score))
+    new_rating1 = round(player1_rating + K_FACTOR * (game_result[0] - expected_score))
     # Player 2
     expected_score = 1 / (
         1 + 10 ** ((player1_rating - player2_rating) / ELO_COEFFICIENT)
