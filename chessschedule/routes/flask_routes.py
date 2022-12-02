e127a8ec950ec4eaac5de16ce83ddfc41eff43d1
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-12-02 13:23:43 +0000

    realized that the SPA could just be compiled on the spot whenever path is changed. Sorry about the absurd amount of deletions and insertions lol

--- chessschedule/routes/flask_routes.py
+++ chessschedule/routes/flask_routes.py
@@ -2,12 +2,8 @@ from ..app import app
 from flask import render_template
 
 
-@app.route("/compile")
-def home():
-    """Renders the entire Jinja template from templates folder"""
-    return render_template("index.html")
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch_all(path):
-    return app.send_static_file("compiled_frontend/index.html")
+def spa_path(path):
+    """Compiles the HTML then returns it as a static file to allow for SPA paths to work."""
+    return render_template("index.html")
