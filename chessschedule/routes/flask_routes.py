3abeb3afc1062ade454d9ea343dcd73c03f2361d
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-12-02 02:04:31 +0000

    added the compiled_frontend folder. This is the page that is being sent now. This allows us to remove the # that was infront of every single route before

--- chessschedule/routes/flask_routes.py
+++ chessschedule/routes/flask_routes.py
@@ -2,7 +2,12 @@ from ..app import app
 from flask import render_template
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/compile")
 def home():
+    """Renders the entire Jinja template from templates folder"""
     return render_template("index.html")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("compiled_frontend/index.html")
