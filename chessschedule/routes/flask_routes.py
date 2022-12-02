fc366e9c17e35c4d09a566a2a26b0716f806aaf6
clayrock78 <clayrock78@gmail.com>
2022-12-02 13:23:07 +0000

    ran black

--- chessschedule/routes/flask_routes.py
+++ chessschedule/routes/flask_routes.py
@@ -7,7 +7,8 @@ def home():
     """Renders the entire Jinja template from templates folder"""
     return render_template("index.html")
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def catch_all(path):
     return app.send_static_file("compiled_frontend/index.html")
