5d23eb3a880ad3ca62a9d2f32bafac36f2b5b655
clayrock78 <clayrock78@gmail.com>
2022-12-02 20:35:01 +0000

    fixed button sized for skill select on mobile #21

--- chessschedule/models/room.py
+++ chessschedule/models/room.py
@@ -13,8 +13,6 @@ class Room:
         self.uuid = str(uuid1())
         self.admin_uuid = admin_uuid
         self.admin_sid = admin_sid
-
-        # TODO this is a temporary solution - the odds of a collision are low but possible
         self.room_code = str(randint(1_000_000, 9_999_999))
         self.players: List[Player] = list()
         self.player_names = set()
