f54738a030e8f9ee926fdfda6c70187e9a6450e0
Abdullah Mohammed <abdullah.mohammed@students.sacs.k12.in.us>
2023-08-15 09:52:11 -0400

    changed favicon

--- chessschedule/app.py
+++ chessschedule/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
--- chessschedule/routes/flask_routes.py
+++ chessschedule/routes/flask_routes.py
@@ -1,5 +1,6 @@
+import os
 from ..app import app
-from flask import send_from_directory
+from flask import send_from_directory, url_for
 
 # Path for our main Svelte page
 @app.route("/<path:path>")
@@ -12,9 +13,11 @@ def spa(path):
         return app.send_static_file('/'.join(path.split("/")[2:]))
     return send_from_directory('../client/dist', 'index.html')
 
-
-
     
 @app.route('/')
 def base():
     return send_from_directory('../client/dist', 'index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'assets/favicon.ico', mimetype='image/vnd.microsoft.icon')
